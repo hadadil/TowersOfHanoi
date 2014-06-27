54f1e887a9839f87848ea130458c2bef5a89f55c
Peter Baier <LioN@NsG-TeaM.com>
2014-06-27 20:04:50 +0100

    Javadoc + some more tests

--- TowersOfHanoi/src/test/java/com/baier/toh/MoveCounterTest.java
+++ TowersOfHanoi/src/test/java/com/baier/toh/MoveCounterTest.java
@@ -1,23 +1,37 @@
 package com.baier.toh;
 
 import static org.junit.Assert.*;
-import com.baier.toh.MoveCounter;
+
+import javax.naming.TimeLimitExceededException;
 
 import org.junit.Before;
 import org.junit.Test;
 
+
 public class MoveCounterTest {
+	MoveCounter mc1,mc2;
 
 	@Before
 	public void setUp() throws Exception {
-		
+		mc1 = new MoveCounter();
+		mc2 = new MoveCounter();
 	}
 
-
 	@Test
-	public void count_TOH_moves_test() {
-		MoveCounter mc = new MoveCounter();
-		assertEquals(31, mc.countMovesForDisc(5));
+	public void towersOfHanoi_5discTest() {
+		assertEquals(31, mc1.countMovesForDisc(5));
+	}
+	
+	@Test
+	public void towersOfHanoi_MultipleTestWithSameNumber() {
+		assertEquals(7, mc1.countMovesForDisc(3));
+		assertEquals(7, mc2.countMovesForDisc(3));	
+	}
+	
+	@Test(timeout=30000)
+	public void towersOfHanoi_64disc() throws TimeLimitExceededException {
+			assertEquals("will fail", 31, mc1.countMovesForDisc(64));
+		
 	}
 
 	
